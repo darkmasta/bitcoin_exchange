2bed784c62f6fb559e9c408cb674423578606179
darkmasta <alperen_bagci@hotmail.com>
2013-03-30 12:10:00 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BitcoinExchange::Application.config.secret_token = '8107ecb0853a68bbba235c6d9f977ec249a462d394c3aad7cc29f794659135c841f4bbab7288cf333cc79dd371390f890324785d7c9cd4357f37e72c2328910c'
